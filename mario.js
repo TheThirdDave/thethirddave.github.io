b825cb8734b66dacbb32dce62a00c17f7149be58
thethirddave <phida11@uw.edu>
2015-06-01 23:49:11 -0700

    testing touch controls

--- mario.js
+++ mario.js
@@ -5,64 +5,69 @@ var timeoutReference;
 
 $(function(){
 
-  $( "#grid" ).mousedown(function(event) {
+  $( "#grid" ).on('touchstart mousedown', (function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    if (event.handled !== true) {
 
-    console.log("" + event.target.getAttribute("data-pic-file"))
-
-    if (first) {
-      $("#status").html("" + event.target.title);
-    }
+      if (first) {
+        $("#status").html("" + event.target.title);
+      }
 
 
-    if(first == false) {
-      $("#status").html("Action");
-      $("#grid").css( 'background', 'url("action.png") no-repeat center center fixed');
-      setTimeout(function() {
-        if ($("#status").html() != "") {
-          $("#status").html(event.target.title)
-          $("#grid").css( 'background', 'url("' + event.target.getAttribute("data-pic-file") + '")  no-repeat center center fixed' );
-        }
-      }, 500);
-    }
+      if(first == false) {
+        $("#status").html("Action");
+        $("#grid").css( 'background', 'url("action.png") no-repeat center center fixed');
+        setTimeout(function() {
+          if ($("#status").html() != "") {
+            $("#status").html(event.target.title)
+            $("#grid").css( 'background', 'url("' + event.target.getAttribute("data-pic-file") + '")  no-repeat center center fixed' );
+          }
+        }, 500);
+      }
 
-    clicked = true;
-    if(clicked ==true) {
-      $("#box1").mouseenter(function(){
-        $("#status").html("Jump Left");
-        $("#grid").css( 'background', 'url("jumpleft.png")  no-repeat center center fixed' );
-      });
-      $("#box2").mouseenter(function(){
-        $("#status").html("Jump");
-        $("#grid").css( 'background', 'url("jump.png")  no-repeat center center fixed' );
-      });
-      $("#box3").mouseenter(function(){
-        $("#status").html("Jump Right");
-        $("#grid").css( 'background', 'url("jumpright.png")  no-repeat center center fixed' );
-      });
-      $("#box4").mouseenter(function(){
-       $("#status").html("Backward");
-       $("#grid").css( 'background', 'url("backward.png")  no-repeat center center fixed' );
-      });
-      $("#box5").mouseenter(function(){
-        $("#status").html("");
-        $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
-      });
-      $("#box6").mouseenter(function(){
-        $("#status").html("Forward");
-        $("#grid").css( 'background', 'url("forward.png")  no-repeat center center fixed' );
-      });
-      $("#box7").mouseenter(function(){
-        $("#status").html("");
-        $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
-      });
-      $("#box8").mouseenter(function(){
-        $("#status").html("Crouch");
-        $("#grid").css( 'background', 'url("down.png")  no-repeat center center fixed' );
-      });
-      $("#box9").mouseenter(function(){
-        $("#status").html("");
-        $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
-      });
+      clicked = true;
+      if(clicked ==true) {
+        $("#box1").mouseenter(function(){
+          $("#status").html("Jump Left");
+          $("#grid").css( 'background', 'url("jumpleft.png")  no-repeat center center fixed' );
+        });
+        $("#box2").mouseenter(function(){
+          $("#status").html("Jump");
+          $("#grid").css( 'background', 'url("jump.png")  no-repeat center center fixed' );
+        });
+        $("#box3").mouseenter(function(){
+          $("#status").html("Jump Right");
+          $("#grid").css( 'background', 'url("jumpright.png")  no-repeat center center fixed' );
+        });
+        $("#box4").mouseenter(function(){
+         $("#status").html("Backward");
+         $("#grid").css( 'background', 'url("backward.png")  no-repeat center center fixed' );
+        });
+        $("#box5").mouseenter(function(){
+          $("#status").html("");
+          $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
+        });
+        $("#box6").mouseenter(function(){
+          $("#status").html("Forward");
+          $("#grid").css( 'background', 'url("forward.png")  no-repeat center center fixed' );
+        });
+        $("#box7").mouseenter(function(){
+          $("#status").html("");
+          $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
+        });
+        $("#box8").mouseenter(function(){
+          $("#status").html("Crouch");
+          $("#grid").css( 'background', 'url("down.png")  no-repeat center center fixed' );
+        });
+        $("#box9").mouseenter(function(){
+          $("#status").html("");
+          $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
+        });
+      }
+      event.handled = true;
+    } else {
+      return false;
     }
   });
 });
