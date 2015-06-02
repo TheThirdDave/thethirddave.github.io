627d6a4d0e9aff1261ea24b163fe4313a7b87ec9
thethirddave <phida11@uw.edu>
2015-06-02 01:46:34 -0700

    added comments and cleaned up code

--- mario.js
+++ mario.js
@@ -2,19 +2,20 @@ var clicked = false;
 var first = true;
 var timeoutReference;
 
-
+// handles the mousedown and touchstart events for gestures
 $(function(){
-
   $( "#grid" ).on('touchstart mousedown', function(event) {
     event.stopPropagation();
     event.preventDefault();
     if (event.handled !== true) {
 
+      // handles the case when it is the first click/touch from a user
       if (first) {
         $("#status").html("" + event.target.title);
       }
 
-
+      //looks for if this ISN'T the first time the click/touch event has fired since a pause
+      //if it is not then fires an action content
       if(first == false) {
         $("#status").html("Action");
         $("#grid").css( 'background', 'url("action.png") no-repeat center center fixed');
@@ -26,41 +27,44 @@ $(function(){
         }, 500);
       }
 
+
+      //could probably be cleaned up into just one function but I'm too lazy right now
+      //handles the mouse enter states
       clicked = true;
       if(clicked ==true) {
-        $("#box1").on('touchenter mouseenter', function(){
+        $("#box1").on('mouseenter', function(){
           $("#status").html("Jump Left");
           $("#grid").css( 'background', 'url("jumpleft.png")  no-repeat center center fixed');
         });
-        $("#box2").on('touchenter mouseenter', function(){
+        $("#box2").on('mouseenter', function(){
           $("#status").html("Jump");
           $("#grid").css( 'background', 'url("jump.png")  no-repeat center center fixed' );
         });
-        $("#box3").on('touchenter mouseenter', function(){
+        $("#box3").on('mouseenter', function(){
           $("#status").html("Jump Right");
           $("#grid").css( 'background', 'url("jumpright.png")  no-repeat center center fixed' );
         });
-        $("#box4").on('touchenter mouseenter', function(){
+        $("#box4").on('mouseenter', function(){
          $("#status").html("Backward");
          $("#grid").css( 'background', 'url("backward.png")  no-repeat center center fixed' );
         });
-        $("#box5").on('touchenter mouseenter', function(){
+        $("#box5").on('mouseenter', function(){
           $("#status").html("");
           $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
         });
-        $("#box6").on('touchenter mouseenter', function(){
+        $("#box6").on('mouseenter', function(){
           $("#status").html("Forward");
           $("#grid").css( 'background', 'url("forward.png")  no-repeat center center fixed' );
         });
-        $("#box7").on('touchenter mouseenter', function(){
+        $("#box7").on('mouseenter', function(){
           $("#status").html("");
           $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
         });
-        $("#box8").on('touchenter mouseenter', function(){
+        $("#box8").on('mouseenter', function(){
           $("#status").html("Crouch");
           $("#grid").css( 'background', 'url("down.png")  no-repeat center center fixed' );
         });
-        $("#box9").on('touchenter mouseenter', function(){
+        $("#box9").on('mouseenter', function(){
           $("#status").html("");
           $("#grid").css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
         });
@@ -72,13 +76,15 @@ $(function(){
   });
 });
 
+//For touch devices to be able to interact with device
 $(function() {
   $("#grid").on('touchmove', function(event) {
     event.preventDefault();
+    //grab touch event
     var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouces[0];
+    //get the dom element that current touch is over
     var current = document.elementFromPoint(touch.pageX, touch.pageY);
-    console.log(current.title);
-    console.log(touch.pageX + " " + touch.pageY);
+    //display corresponding content
     $("#status").html(current.title);
     $("#grid").css( 'background', 'url("' + current.getAttribute("data-pic-file") + '")  no-repeat center center fixed' );
   });
@@ -87,9 +93,11 @@ $(function() {
 $(function() {
 
   $("#grid").on('touchend mouseup', function() {
+    //resets the timeout timer
     window.clearTimeout(timeoutReference);
     clicked=false;
     first=false;
+    //turns off events so they only happen when a click our touch is happening
     $("#box1").off();
     $("#box2").off();
     $("#box3").off();
@@ -99,8 +107,10 @@ $(function() {
     $("#box7").off();
     $("#box8").off();
     $("#box9").off();
+    //reset the conent to a neutral state
     $("#status").html("");
     $( "#grid" ).css( 'background', 'url("neutral.png")  no-repeat center center fixed' );
+    //this is so the pause has a slight delay; allows for action clicks to happen
     timeoutReference = setTimeout(function() {
       if(clicked==false) { 
         $( "#grid" ).css( 'background', 'url("pause.png")  no-repeat center center fixed' );
